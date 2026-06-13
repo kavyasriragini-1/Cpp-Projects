9e698cf2896265c2bbbe73fd1f32dbf5200c77dc
kavyasriragini-1 <kavyasriragini@gmail.com>
2026-06-13 16:54:59 +0530

    Updated grade calculator project

--- grade_calculator.cpp
+++ grade_calculator.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
-uisng namespace std;
+using namespace std;
 int main() {
-
+    int marks;
+    cout<<"Enter your marks: ";
+    cin>>marks;
+    if(marks>=90)
+        cout<<"Grade: 'O'\n";
+    else if(marks>=80)
+        cout<<"Grade: 'A+'\n";
+    else if(marks>=70)
+        cout<<"Grade: 'A'\n";
+    else if(marks>=60)
+        cout<<"Grade: 'B+'\n";
+    else if(marks>=50)
+        cout<<"Grade: 'B'\n";
+    else
+        cout<<"FAIL\n";
 }
